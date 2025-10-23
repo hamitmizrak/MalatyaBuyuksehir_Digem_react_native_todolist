3de61b6fe7bb592aa2f61e9ebb536188af6419c3
hamitmizrak <hamitmizrak@gmail.com>
2025-10-23 11:14:19 +0300

    import React,

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,3 +3,16 @@
 - Firebase yoktur
 • İsteğe bağlı kalıcılık (AsyncStorage) için en altta yorumlu şablon var.
 */
+
+import React, { useCallback, useMemo, useState } from 'react';
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
