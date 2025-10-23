752a692191db2e692c95c7eb26f6eff0244f1bc9
hamitmizrak <hamitmizrak@gmail.com>
2025-10-23 11:21:35 +0300

    react-native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 // index.tsx 
 /*
-- Firebase yoktur
+• Firebase yoktur
 • İsteğe bağlı kalıcılık (AsyncStorage) için en altta yorumlu şablon var.
 */
 
+// React temel bileşen ve React Native bileşenleri içe aktarılıyor
 import React, { useCallback, useMemo, useState } from 'react';
+
+// React Native bileşenleri içe aktarılıyor.
 import {
   FlatList,
   KeyboardAvoidingView,
